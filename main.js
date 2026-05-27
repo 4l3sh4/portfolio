4183cdd01859598a23b4e3546f847539740b945c
Alesha <aleshanoorsalim@gmail.com>
2026-05-28 05:47:48 +0800

    First batch of code, done!

---
+++ main.js
@@ -0,0 +1,26 @@
+function myFunction1() {
+    var x = document.getElementById("myDIV1");
+    var y = document.getElementById("myDIV2");
+    var z = document.getElementById("myDIV3");
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+}
+
+function myFunction2() {
+    var x = document.getElementById("myDIV1");
+    var y = document.getElementById("myDIV2");
+    var z = document.getElementById("myDIV3");
+    y.style.display = "block";
+    x.style.display = "none";
+    z.style.display = "none";
+}
+
+function myFunction3() {
+    var x = document.getElementById("myDIV1");
+    var y = document.getElementById("myDIV2");
+    var z = document.getElementById("myDIV3");
+    z.style.display = "block";
+    x.style.display = "none";
+    y.style.display = "none";
+}
