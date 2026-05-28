d13e5537b49cc4ceed832af501ad19330ffbf0fb
Alesha <aleshanoorsalim@gmail.com>
2026-05-29 06:02:16 +0800

    Second batch of coding!

--- main.js
+++ main.js
@@ -1,26 +1,64 @@
 function myFunction1() {
-    var x = document.getElementById("myDIV1");
-    var y = document.getElementById("myDIV2");
-    var z = document.getElementById("myDIV3");
-    x.style.display = "block";
+    var v = document.getElementById("myDIV5");
+    var w = document.getElementById("myDIV1");
+    var x = document.getElementById("myDIV2");
+    var y = document.getElementById("myDIV3");
+    var z = document.getElementById("myDIV4");
+    w.style.display = "block";
+    v.style.display = "none";
+    x.style.display = "none";
     y.style.display = "none";
     z.style.display = "none";
 }
 
 function myFunction2() {
-    var x = document.getElementById("myDIV1");
-    var y = document.getElementById("myDIV2");
-    var z = document.getElementById("myDIV3");
-    y.style.display = "block";
-    x.style.display = "none";
+    var w = document.getElementById("myDIV1");
+    var x = document.getElementById("myDIV2");
+    var y = document.getElementById("myDIV3");
+    var z = document.getElementById("myDIV4");
+    var v = document.getElementById("myDIV5");
+    x.style.display = "block";
+    w.style.display = "none";
+    y.style.display = "none";
     z.style.display = "none";
+    v.style.display = "none";
 }
 
 function myFunction3() {
-    var x = document.getElementById("myDIV1");
-    var y = document.getElementById("myDIV2");
-    var z = document.getElementById("myDIV3");
+    var w = document.getElementById("myDIV1");
+    var x = document.getElementById("myDIV2");
+    var y = document.getElementById("myDIV3");
+    var z = document.getElementById("myDIV4");
+    var v = document.getElementById("myDIV5");
+    y.style.display = "block";
+    w.style.display = "none";
+    x.style.display = "none";
+    z.style.display = "none";
+    v.style.display = "none";
+}
+
+function myFunction4() {
+    var w = document.getElementById("myDIV1");
+    var x = document.getElementById("myDIV2");
+    var y = document.getElementById("myDIV3");
+    var z = document.getElementById("myDIV4");
+    var v = document.getElementById("myDIV5");
     z.style.display = "block";
+    w.style.display = "none";
     x.style.display = "none";
     y.style.display = "none";
+    v.style.display = "none";
+}
+
+function myFunction5() {
+    var w = document.getElementById("myDIV1");
+    var x = document.getElementById("myDIV2");
+    var y = document.getElementById("myDIV3");
+    var z = document.getElementById("myDIV4");
+    var v = document.getElementById("myDIV5");
+    v.style.display = "block";
+    w.style.display = "none";
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";
 }
