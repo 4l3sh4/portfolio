71df2e69066c81bb6f890dcad87c43b8ab50b7e9
Alesha <aleshanoorsalim@gmail.com>
2026-05-29 08:05:38 +0800

    Finished!

--- main.js
+++ main.js
@@ -1,64 +1,40 @@
-function myFunction1() {
-    var v = document.getElementById("myDIV5");
-    var w = document.getElementById("myDIV1");
-    var x = document.getElementById("myDIV2");
-    var y = document.getElementById("myDIV3");
-    var z = document.getElementById("myDIV4");
-    w.style.display = "block";
-    v.style.display = "none";
-    x.style.display = "none";
-    y.style.display = "none";
-    z.style.display = "none";
+function showSection(sectionId, tabElement) {
+    var sections = document.querySelectorAll(".section-panel");
+    var tabs = document.querySelectorAll(".box-tab");
+
+    sections.forEach(function (section) {
+        if (section.id === sectionId) {
+            section.classList.remove("is-hidden");
+            section.classList.add("is-visible");
+        } else {
+            section.classList.remove("is-visible");
+            section.classList.add("is-hidden");
+        }
+    });
+
+    tabs.forEach(function (tab) {
+        tab.classList.toggle("active-tab", tab === tabElement);
+    });
+
+    var targetSection = document.getElementById(sectionId);
+
+    if (targetSection) {
+        requestAnimationFrame(function () {
+            targetSection.scrollIntoView({ behavior: "smooth", block: "start" });
+        });
+    }
 }
 
-function myFunction2() {
-    var w = document.getElementById("myDIV1");
-    var x = document.getElementById("myDIV2");
-    var y = document.getElementById("myDIV3");
-    var z = document.getElementById("myDIV4");
-    var v = document.getElementById("myDIV5");
-    x.style.display = "block";
-    w.style.display = "none";
-    y.style.display = "none";
-    z.style.display = "none";
-    v.style.display = "none";
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var defaultTab = document.querySelector(".box-tab");
+    var defaultSection = document.getElementById("myDIV1");
 
-function myFunction3() {
-    var w = document.getElementById("myDIV1");
-    var x = document.getElementById("myDIV2");
-    var y = document.getElementById("myDIV3");
-    var z = document.getElementById("myDIV4");
-    var v = document.getElementById("myDIV5");
-    y.style.display = "block";
-    w.style.display = "none";
-    x.style.display = "none";
-    z.style.display = "none";
-    v.style.display = "none";
-}
+    if (defaultTab) {
+        defaultTab.classList.add("active-tab");
+    }
 
-function myFunction4() {
-    var w = document.getElementById("myDIV1");
-    var x = document.getElementById("myDIV2");
-    var y = document.getElementById("myDIV3");
-    var z = document.getElementById("myDIV4");
-    var v = document.getElementById("myDIV5");
-    z.style.display = "block";
-    w.style.display = "none";
-    x.style.display = "none";
-    y.style.display = "none";
-    v.style.display = "none";
-}
-
-function myFunction5() {
-    var w = document.getElementById("myDIV1");
-    var x = document.getElementById("myDIV2");
-    var y = document.getElementById("myDIV3");
-    var z = document.getElementById("myDIV4");
-    var v = document.getElementById("myDIV5");
-    v.style.display = "block";
-    w.style.display = "none";
-    x.style.display = "none";
-    y.style.display = "none";
-    z.style.display = "none";
-}
+    if (defaultSection) {
+        defaultSection.classList.remove("is-hidden");
+        defaultSection.classList.add("is-visible");
+    }
+});
